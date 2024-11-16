71d3412be3fce39c909823f868b918f63ce2aaed
Abdul Kabeer <kabeer6017@gmail.com>
2024-11-16 19:59:27 +0500

    Add files via upload

---
+++ PrintArrayWithLoop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int array [20] = {2,5,3,6,8,12,14,56,85,96,45,12,52};
+    int n =20;
+    for(int i=0; i<n; i++){
+        cout<<array[i];
+        cout<< " ";
+    }
+
+    return 0;
+}
